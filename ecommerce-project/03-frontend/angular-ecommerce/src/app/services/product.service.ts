05cf470e468e9fc045bd164c7e80b37b2c358bee
hbao0421 <hbao20000421@gmail.com>
2022-10-05 23:46:21 -0400

    try

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable, pipe } from 'rxjs';
+import { Product } from '../common/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  public baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient:HttpClient) {}
+  getProductList(): Observable<Product[]>{
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+interface GetResponse{
+  _embedded:{
+    products:Product[];
+  }
+}
