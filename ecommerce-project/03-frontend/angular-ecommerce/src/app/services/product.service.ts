b9a769a169c506e1afd5eece35ad8007667adb7a
hbao0421 <hbao20000421@gmail.com>
2022-10-10 23:45:58 -0400

    menu

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -2,22 +2,38 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable, pipe } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  public baseUrl = 'http://localhost:8080/api/products';
 
+  public baseUrl = 'http://localhost:8080/api/products';
+  public categoryUrl = 'http://localhost:8080/api/product-category';
+            
   constructor(private httpClient:HttpClient) {}
-  getProductList(): Observable<Product[]>{
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProductList(theCategoryId:number): Observable<Product[]>{
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
+  getProductCategories():Observable<ProductCategory[]>{
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
 }
-interface GetResponse{
+
+interface GetResponseProducts{
   _embedded:{
     products:Product[];
+  }
+}
+
+interface GetResponseProductCategory{
+  _embedded:{
+    productCategory:ProductCategory[];
   }
 }
