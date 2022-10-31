3a8ddfd58fec93b00a570dc9fdb54fd7f56402e2
hbao0421 <hbao20000421@gmail.com>
2022-10-31 11:37:42 -0400

    country

---
+++ ecommerce-project/02-backend/springboot-ecommerce/springboot-ecommerce/src/main/java/com/hanbao/ecommerce/entity/Country.java
@@ -0,0 +1,21 @@
+package com.hanbao.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "code")
+    private String code;
+    @Column(name = "namne")
+    private String name;
+}
